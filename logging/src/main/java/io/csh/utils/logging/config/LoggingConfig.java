95c3b135aa20ccf3b225f0da82547ac45847358a
csh <csh@daonecns.co.kr>
2025-06-17 13:32:37 +0900

    refactor: logging 모듈에서 파일 관련 코드 제거

--- logging/src/main/java/io/csh/utils/logging/config/LoggingConfig.java
+++ logging/src/main/java/io/csh/utils/logging/config/LoggingConfig.java
@@ -1,191 +1,64 @@
 package io.csh.utils.logging.config;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import io.csh.utils.logging.LogLevel;
-import io.csh.utils.core.config.SystemProperties;
+import io.csh.utils.config.SystemProperties;
+import lombok.Getter;
 
-/**
- * 로깅 설정을 관리하는 클래스
- */
+@Getter
 public class LoggingConfig {
-    private static final LoggingConfig INSTANCE = new LoggingConfig();
-    
-    private String logFilePath;
-    private String logFileName;
-    private String logFileMaxSize;
-    private String logFileMaxTotalSize;
-    private String logFileRetentionDays;
-    private String logFileCompressionUnit;
-    private String logFileCompressionValue;
-    private String logFileCompressionFormat;
-    private String logPattern;
-    
-    private LoggingConfig() {
-        initializeFromProperties();
-    }
-    
-    public static LoggingConfig getInstance() {
-        return INSTANCE;
-    }
-    
-    private void initializeFromProperties() {
-        // SystemProperties를 통해 설정을 가져옴
-        logFilePath = SystemProperties.getProperty(LoggingProperties.LOG_FILE_PATH)
-            .checkSystemProperty()
-            .checkApplicationProperties()
-            .orElse(LoggingProperties.DEFAULT_LOG_FILE_PATH);
-            
-        logFileName = SystemProperties.getProperty(LoggingProperties.LOG_FILE_NAME)
-            .checkSystemProperty()
-            .checkApplicationProperties()
-            .orElse(LoggingProperties.DEFAULT_LOG_FILE_NAME);
-            
-        logFileMaxSize = SystemProperties.getProperty(LoggingProperties.LOG_FILE_MAX_SIZE)
-            .checkSystemProperty()
-            .checkApplicationProperties()
-            .orElse(LoggingProperties.DEFAULT_LOG_FILE_MAX_SIZE);
-            
-        logFileMaxTotalSize = SystemProperties.getProperty(LoggingProperties.LOG_FILE_MAX_TOTAL_SIZE)
-            .checkSystemProperty()
-            .checkApplicationProperties()
-            .orElse(LoggingProperties.DEFAULT_LOG_FILE_MAX_TOTAL_SIZE);
-            
-        logFileRetentionDays = SystemProperties.getProperty(LoggingProperties.LOG_FILE_RETENTION_DAYS)
-            .checkSystemProperty()
-            .checkApplicationProperties()
-            .orElse(LoggingProperties.DEFAULT_LOG_FILE_RETENTION_DAYS);
-            
-        logFileCompressionUnit = SystemProperties.getProperty(LoggingProperties.LOG_FILE_COMPRESSION_UNIT)
-            .checkSystemProperty()
-            .checkApplicationProperties()
-            .orElse(LoggingProperties.DEFAULT_LOG_FILE_COMPRESSION_UNIT);
-            
-        logFileCompressionValue = SystemProperties.getProperty(LoggingProperties.LOG_FILE_COMPRESSION_VALUE)
-            .checkSystemProperty()
-            .checkApplicationProperties()
-            .orElse(LoggingProperties.DEFAULT_LOG_FILE_COMPRESSION_VALUE);
-            
-        logFileCompressionFormat = SystemProperties.getProperty(LoggingProperties.LOG_FILE_COMPRESSION_FORMAT)
-            .checkSystemProperty()
-            .checkApplicationProperties()
-            .orElse(LoggingProperties.DEFAULT_LOG_FILE_COMPRESSION_FORMAT);
-            
-        logPattern = SystemProperties.getProperty(LoggingProperties.LOG_PATTERN)
-            .checkSystemProperty()
-            .checkApplicationProperties()
-            .orElse(LoggingProperties.DEFAULT_LOG_PATTERN);
-    }
-    
-    public Path getLogDirectory() {
-        return Paths.get(logFilePath);
-    }
-    
-    public String getLogFileName() {
-        return logFileName;
-    }
-    
-    public String getLogFileMaxSize() {
-        return logFileMaxSize;
-    }
-    
-    public void setLogFileMaxSize(String logFileMaxSize) {
-        this.logFileMaxSize = logFileMaxSize;
-    }
-    
-    public String getLogFileMaxTotalSize() {
-        return logFileMaxTotalSize;
-    }
-    
-    public void setLogFileMaxTotalSize(String logFileMaxTotalSize) {
-        this.logFileMaxTotalSize = logFileMaxTotalSize;
-    }
-    
-    public String getLogFileRetentionDays() {
-        return logFileRetentionDays;
-    }
-    
-    public void setLogFileRetentionDays(String logFileRetentionDays) {
-        this.logFileRetentionDays = logFileRetentionDays;
-    }
-    
-    public String getLogFileCompressionUnit() {
-        return logFileCompressionUnit;
-    }
-    
-    public void setLogFileCompressionUnit(String logFileCompressionUnit) {
-        this.logFileCompressionUnit = logFileCompressionUnit;
-    }
-    
-    public String getLogFileCompressionValue() {
-        return logFileCompressionValue;
-    }
-    
-    public void setLogFileCompressionValue(String logFileCompressionValue) {
-        this.logFileCompressionValue = logFileCompressionValue;
-    }
-    
-    public String getLogFileCompressionFormat() {
-        return logFileCompressionFormat;
-    }
-    
-    public void setLogFileCompressionFormat(String logFileCompressionFormat) {
-        this.logFileCompressionFormat = logFileCompressionFormat;
+    private final String level;
+    private final boolean consoleEnabled;
+    private final String consolePattern;
+    private final String timestampFormat;
+    private final boolean threadFormat;
+    private final boolean levelFormat;
+    private final boolean loggerFormat;
+    private final boolean messageFormat;
+
+    public LoggingConfig() {
+        this.level = SystemProperties.getProperty(LoggingProperties.LEVEL, LoggingProperties.DEFAULT_LEVEL);
+        this.consoleEnabled = Boolean.parseBoolean(
+            SystemProperties.getProperty(LoggingProperties.CONSOLE_ENABLED, LoggingProperties.DEFAULT_CONSOLE_ENABLED)
+        );
+        this.consolePattern = SystemProperties.getProperty(
+            LoggingProperties.CONSOLE_PATTERN, LoggingProperties.DEFAULT_CONSOLE_PATTERN
+        );
+        this.timestampFormat = SystemProperties.getProperty(
+            LoggingProperties.FORMAT_TIMESTAMP, LoggingProperties.DEFAULT_FORMAT_TIMESTAMP
+        );
+        this.threadFormat = Boolean.parseBoolean(
+            SystemProperties.getProperty(LoggingProperties.FORMAT_THREAD, LoggingProperties.DEFAULT_FORMAT_THREAD)
+        );
+        this.levelFormat = Boolean.parseBoolean(
+            SystemProperties.getProperty(LoggingProperties.FORMAT_LEVEL, LoggingProperties.DEFAULT_FORMAT_LEVEL)
+        );
+        this.loggerFormat = Boolean.parseBoolean(
+            SystemProperties.getProperty(LoggingProperties.FORMAT_LOGGER, LoggingProperties.DEFAULT_FORMAT_LOGGER)
+        );
+        this.messageFormat = Boolean.parseBoolean(
+            SystemProperties.getProperty(LoggingProperties.FORMAT_MESSAGE, LoggingProperties.DEFAULT_FORMAT_MESSAGE)
+        );
     }
 
-    public LogLevel getLogLevel() {
-        String level = SystemProperties.getProperty(LoggingProperties.LOG_LEVEL)
-            .checkSystemProperty()
-            .checkApplicationProperties()
-            .orElse(LoggingProperties.DEFAULT_LOG_LEVEL);
-        return LogLevel.valueOf(level);
-    }
-
-    public boolean isConsoleEnabled() {
-        String value = SystemProperties.getProperty(LoggingProperties.LOG_CONSOLE_ENABLED)
-            .checkSystemProperty()
-            .checkApplicationProperties()
-            .orElse(LoggingProperties.DEFAULT_LOG_CONSOLE_ENABLED);
-        return Boolean.parseBoolean(value);
-    }
-
-    public String getLogPattern() {
-        return logPattern;
-    }
-
-    public void setLogPattern(String logPattern) {
-        this.logPattern = logPattern;
-    }
-
-    public void setLogFilePath(String logFilePath) {
-        this.logFilePath = logFilePath;
-    }
-
-    public void setLogFileName(String logFileName) {
-        this.logFileName = logFileName;
-    }
-
-    /**
-     * 현재 로깅 설정 상태를 출력합니다.
-     * @return 로깅 설정 상태 문자열
-     */
-    public String printLoggingStatus() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("=== Logging Configuration Status ===\n");
-        sb.append("Log Level: ").append(getLogLevel()).append("\n");
-        sb.append("Console Enabled: ").append(isConsoleEnabled()).append("\n");
-        sb.append("Log File Path: ").append(logFilePath).append("\n");
-        sb.append("Log File Name: ").append(logFileName).append("\n");
-        sb.append("Log File Max Size: ").append(logFileMaxSize).append("\n");
-        sb.append("Log File Max Total Size: ").append(logFileMaxTotalSize).append("\n");
-        sb.append("Log File Retention Days: ").append(logFileRetentionDays).append("\n");
-        sb.append("Log File Compression: ").append(logFileCompressionUnit)
-          .append(" ").append(logFileCompressionValue)
-          .append(" (").append(logFileCompressionFormat).append(")\n");
-        sb.append("Log Pattern: ").append(logPattern).append("\n");
-        sb.append("Overwrite: ").append(System.getProperty("csh.logging.overwrite", "false")).append("\n");
-        sb.append("===================================");
-        return sb.toString();
+    public String getFormattedConfig() {
+        return String.format("""
+            Logging Configuration:
+            - Level: %s
+            - Console Enabled: %s
+            - Console Pattern: %s
+            - Timestamp Format: %s
+            - Thread Format: %s
+            - Level Format: %s
+            - Logger Format: %s
+            - Message Format: %s
+            """,
+            level,
+            consoleEnabled,
+            consolePattern,
+            timestampFormat,
+            threadFormat,
+            levelFormat,
+            loggerFormat,
+            messageFormat
+        );
     }
 } 
--- logging/src/main/java/io/csh/utils/logging/config/LoggingProperties.java
+++ logging/src/main/java/io/csh/utils/logging/config/LoggingProperties.java
@@ -1,46 +1,32 @@
 package io.csh.utils.logging.config;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 /**
  * 로깅 관련 프로퍼티 키와 기본값을 정의하는 클래스
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class LoggingProperties {
-    private LoggingProperties() {
-        throw new AssertionError("Utility class");
-    }
+    // 로깅 레벨
+    public static final String LEVEL = "logging.level";
+    public static final String DEFAULT_LEVEL = "INFO";
 
-    // 로그 레벨 설정
-    public static final String LOG_LEVEL = "csh.logging.level";
-    public static final String DEFAULT_LOG_LEVEL = "INFO";
+    // 콘솔 출력
+    public static final String CONSOLE_ENABLED = "logging.console.enabled";
+    public static final String DEFAULT_CONSOLE_ENABLED = "true";
+    public static final String CONSOLE_PATTERN = "logging.console.pattern";
+    public static final String DEFAULT_CONSOLE_PATTERN = "%d{yyyy-MM-dd HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n";
 
-    // 로그 포맷 설정
-    public static final String LOG_PATTERN = "csh.logging.pattern";
-    public static final String DEFAULT_LOG_PATTERN = "%d{yyyy-MM-dd HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n";
-
-    // 로그 파일 설정
-    public static final String LOG_FILE_PATH = "csh.logging.file.path";
-    public static final String LOG_FILE_NAME = "csh.logging.file.name";
-    public static final String LOG_FILE_MAX_SIZE = "csh.logging.file.max-size";
-    public static final String LOG_FILE_MAX_TOTAL_SIZE = "csh.logging.file.max-total-size";
-    public static final String LOG_FILE_RETENTION_DAYS = "csh.logging.file.retention-days";
-    public static final String LOG_FILE_COMPRESSION_UNIT = "csh.logging.file.compression.unit";
-    public static final String LOG_FILE_COMPRESSION_VALUE = "csh.logging.file.compression.value";
-    public static final String LOG_FILE_COMPRESSION_FORMAT = "csh.logging.file.compression.format";
-
-    // 기본값
-    public static final String DEFAULT_LOG_FILE_PATH = "logs/csh-utils";
-    public static final String DEFAULT_LOG_FILE_NAME = "csh-utils.log";
-    public static final String DEFAULT_LOG_FILE_MAX_SIZE = "10MB";
-    public static final String DEFAULT_LOG_FILE_MAX_TOTAL_SIZE = "1GB";
-    public static final String DEFAULT_LOG_FILE_RETENTION_DAYS = "365";
-    public static final String DEFAULT_LOG_FILE_COMPRESSION_UNIT = "WEEK";
-    public static final String DEFAULT_LOG_FILE_COMPRESSION_VALUE = "1";
-    public static final String DEFAULT_LOG_FILE_COMPRESSION_FORMAT = "gz";
-
-    // 콘솔 출력 설정
-    public static final String LOG_CONSOLE_ENABLED = "csh.logging.console.enabled";
-    public static final String DEFAULT_LOG_CONSOLE_ENABLED = "true";
-
-    // 파일 덮어쓰기 설정
-    public static final String LOG_OVERWRITE = "csh.logging.overwrite";
-    public static final String DEFAULT_LOG_OVERWRITE = "false";
+    // 로그 포맷
+    public static final String FORMAT_TIMESTAMP = "logging.format.timestamp";
+    public static final String DEFAULT_FORMAT_TIMESTAMP = "yyyy-MM-dd HH:mm:ss.SSS";
+    public static final String FORMAT_THREAD = "logging.format.thread";
+    public static final String DEFAULT_FORMAT_THREAD = "true";
+    public static final String FORMAT_LEVEL = "logging.format.level";
+    public static final String DEFAULT_FORMAT_LEVEL = "true";
+    public static final String FORMAT_LOGGER = "logging.format.logger";
+    public static final String DEFAULT_FORMAT_LOGGER = "true";
+    public static final String FORMAT_MESSAGE = "logging.format.message";
+    public static final String DEFAULT_FORMAT_MESSAGE = "true";
 } 
